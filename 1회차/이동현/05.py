ce55c3cd79ccd01555c6111655503a592df157b7
soohofather <enthchal@gmail.com>
2022-07-15 17:11:44 +0900

    1기_1회차_이동현 // 4번까지 풀었습니다

--- 1회차/이동현/05.py
+++ 1회차/이동현/05.py
@@ -5,7 +5,22 @@ from pprint import pprint
 def movie_info(movie, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+a = open('./data/movie.json', 'r', encoding='utf-8')
+b = open('./data/genres.json', 'r', encoding='utf-8')
+
+m = json.load(a)
+n = json.load(b)
+
+finding = ['id', 'title', 'vote_average', 'overview', 'genre_ids']
+dict = {}
+
+for mm in m:
+    if mm in finding:
+       dict[mm] = dict.get(mm, m[mm])
+
+
+
+pprint(dict)
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
