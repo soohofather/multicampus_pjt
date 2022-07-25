262250f149228e5d9bd826f13a5f5b550709dd40
soohofather <enthchal@gmail.com>
2022-07-25 15:54:41 +0900

    1기_1회차_이동현 7월 25일 실습

--- 1회차/이동현/20220725/2_두개뽑아서더하기.py
+++ 1회차/이동현/20220725/2_두개뽑아서더하기.py
@@ -1,8 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/68644
 def solution(numbers):
     answer = []
+    for i in range(0, len(numbers) - 1):
+        for ii in range(i + 1, len(numbers)):
+            answer.append(numbers[i] + numbers[ii])
+            answer = sorted(list(set(answer)))
     return answer
-
 
 print(solution([2, 1, 3, 4, 1]))
 print(solution([5, 0, 2, 7]))
--- 1회차/이동현/20220725/3_OX퀴즈.py
+++ 1회차/이동현/20220725/3_OX퀴즈.py
@@ -2,3 +2,17 @@
 import sys
 
 sys.stdin = open("3_OX퀴즈.txt")
+
+T = int(input())
+
+for i in range(0, T):
+    oxox = input()
+    cnt = 0
+    result = 0
+    for ii in oxox:
+        if ii == 'O':
+            cnt += 1
+            result += cnt
+        else:
+            cnt = 0
+    print(result)
