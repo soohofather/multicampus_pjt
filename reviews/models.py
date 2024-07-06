4f4b3f4e420fe1d33f8883c2b2df305bbb982a47
이동현 <enthchal@gmail.com>
2024-07-06 23:45:07 +0900

    Squashed 'pjt_Django/' content from commit f8aab59
    git-subtree-dir: pjt_Django
    git-subtree-split: f8aab595d0a69ed8c2f019743ca95d816df3e2e8

---
+++ reviews/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+
+
+# Create your models here.
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=80)
+    grade = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
